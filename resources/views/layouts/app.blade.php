3e5394324d8cf7535d1d548b6c724b00e65ecd48
Meet Vora <125785245+MeetVora79@users.noreply.github.com>
2024-04-05 10:51:10 +0530

    A short changes in app.blade.php view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
   <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
   <title>@yield('title')</title>
-   <!-- CSRF Token -->
-   <meta name="csrf-token" content="{{ csrf_token() }}">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <!-- General CSS Files -->
   <link rel="stylesheet" href="{{ asset('backend/assets/modules/bootstrap/css/bootstrap.min.css')}}">
   <link rel="stylesheet" href="{{ asset('backend/assets/modules/fontawesome/css/all.min.css')}}">
@@ -18,13 +19,13 @@
   <!-- Template CSS -->
   <link rel="stylesheet" href="{{ asset('backend/assets/css/style.css')}}">
   <link rel="stylesheet" href="{{ asset('backend/assets/css/components.css')}}">
-    @stack('styles')
+  @stack('styles')
 </head>
 
 <body>
   <div id="app">
     @yield('content')
-   
+
   </div>
 
   <!-- General JS Scripts -->
@@ -43,6 +44,21 @@
   <!-- Template JS File -->
   <script src="{{ asset('backend/assets/js/scripts.js')}}"></script>
   <script src="{{ asset('backend/assets/js/custom.js')}}"></script>
+  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+  @if (session('success'))
+  <script>
+    swal("{{ session('success') }}");
+  </script>
+  @endif
+
+  @if (session('error'))
+  <script>
+    swal("{{ session('error') }}");
+  </script>
+  @endif
+  
   @stack('scripts')
 </body>
+
 </html>
